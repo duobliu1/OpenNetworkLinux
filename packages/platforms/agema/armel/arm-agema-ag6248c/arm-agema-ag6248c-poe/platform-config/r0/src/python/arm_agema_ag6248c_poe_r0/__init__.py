9eeff5f0e4779f5331ab657b9881aeece11cc42a
Dave Hu <dave.hu@agemasystems.com>
2017-03-10 17:53:11 -0500

    arm agema ag6248c onlp first version

--- packages/platforms/agema/armel/arm-agema-ag6248c/arm-agema-ag6248c-poe/platform-config/r0/src/python/arm_agema_ag6248c_poe_r0/__init__.py
+++ packages/platforms/agema/armel/arm-agema-ag6248c/arm-agema-ag6248c-poe/platform-config/r0/src/python/arm_agema_ag6248c_poe_r0/__init__.py
@@ -8,3 +8,16 @@ class OnlPlatform_arm_agema_ag6248c_poe_r0(OnlPlatformAgema):
     PORT_COUNT=50
     PORT_CONFIG="48x1 + 2x10"
 
+    def baseconfig(self):
+        self.insmod('arm-agema-ag6248c-poe-cpld-mux-1.ko')
+        self.insmod('arm-agema-ag6248c-poe-cpld-mux-2.ko')
+		
+        self.new_i2c_devices(
+            [
+                # initiate lm75
+                ('tmp75', 0x49, 0),
+                ('tmp75', 0x4a, 0),
+
+            ]
+        )
+        return True
--- packages/platforms/agema/armel/arm-agema-ag6248c/arm-agema-ag6248c/platform-config/r0/src/python/arm_agema_ag6248c_r0/__init__.py
+++ packages/platforms/agema/armel/arm-agema-ag6248c/arm-agema-ag6248c/platform-config/r0/src/python/arm_agema_ag6248c_r0/__init__.py
@@ -3,8 +3,23 @@ from onl.platform.agema import *
 
 class OnlPlatform_arm_agema_ag6248c_r0(OnlPlatformAgema):
     PLATFORM='arm-agema-ag6248c-r0'
-    MODEL="AG6248C-POE"
-    SYS_OBJECT_ID=".6248.1"
+    MODEL="AG6248C"
+    SYS_OBJECT_ID=".6248.2"
     PORT_COUNT=50
     PORT_CONFIG="48x1 + 2x10"
 
+    def baseconfig(self):
+        self.insmod('arm-agema-ag6248c-poe-cpld-mux-1.ko')
+        self.insmod('arm-agema-ag6248c-poe-cpld-mux-2.ko')
+        
+        self.new_i2c_devices(
+            [
+                # initiate lm75
+                ('tmp75', 0x49, 0),
+                ('tmp75', 0x4a, 0),
+               
+
+                
+            ]
+        )
+        return True	
