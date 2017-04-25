449bb07a635336e122f523a3f1f80bff2137f714
Dave Hu <dave.hu@agemasystems.com>
2017-04-25 05:42:15 -0400

    change the platform name from agema to delta

---
+++ packages/platforms/delta/x86-64/x86-64-delta-ag7648/onlp/builds/src/module/inc/x86_64_delta_ag7648/x86_64_delta_ag7648_dox.h
@@ -0,0 +1,26 @@
+/**************************************************************************//**
+ *
+ * x86_64_delta_ag7648 Doxygen Header
+ *
+ *****************************************************************************/
+#ifndef __x86_64_delta_ag7648_DOX_H__
+#define __x86_64_delta_ag7648_DOX_H__
+
+/**
+ * @defgroup x86_64_delta_ag7648 x86_64_delta_ag7648 - x86_64_delta_ag7648 Description
+ *
+
+The documentation overview for this module should go here.
+
+ *
+ * @{
+ *
+ * @defgroup x86_64_delta_ag7648-x86_64_delta_ag7648 Public Interface
+ * @defgroup x86_64_delta_ag7648-config Compile Time Configuration
+ * @defgroup x86_64_delta_ag7648-porting Porting Macros
+ *
+ * @}
+ *
+ */
+
+#endif /* __x86_64_delta_ag7648_DOX_H__ */
